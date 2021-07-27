6491a477a2934e37d49ef736315b85ab1435324d
bagaszuhri <bagaszuhri@gmail.com>
2021-07-27 09:10:41 +0700

    + form input, add-btn, styling

--- counter-starter/src/App.js
+++ counter-starter/src/App.js
@@ -8,14 +8,19 @@ import  shoppingIcon from './assets/shopping-icon.svg';
 function App() {
   return (
     <>
+
       <nav className="nav">
         <img className="nav-icon" src={shoppingIcon} alt="shopping icon"/>
         <h1 className="nav-title">Shopping List</h1>
       </nav>
 
       <section className="container">
-
+        <form className="form">
+          <input className="input" type="text" placeholder="list" />
+          <button className="add-button" type="submit">Add</button>
+        </form>
       </section>
+
     </>
   );
 }
