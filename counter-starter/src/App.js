1b4a8aeeee241164c3ce6dac16ea286d9a844d38
bagaszuhri <bagaszuhri@gmail.com>
2021-07-29 12:12:41 +0700

    + create searchinput component

--- counter-starter/src/App.js
+++ counter-starter/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 import Container from './components/Container';
+import SearchInput from './components/SearchInput'
 
 import plusIcon from './assets/plus-icon.svg';
 import minusIcon from './assets/minus-icon.svg';
@@ -73,16 +74,11 @@ function App() {
       <Navbar />
 
       <Container>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            onChange={(e) =>{setValue(e.target.value)}}
-            value={value} 
-            className="input" 
-            type="text" 
-            placeholder="List" 
-          />
-          <button className="add-button" type="submit">Add</button>
-        </form>
+        <SearchInput
+          onSubmit={handleSubmit}
+          onChange={(e) =>{setValue(e.target.value)}}
+          value={value}
+        />        
       
       <div className="info">
         <div className="info-total">
