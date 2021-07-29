32ede43d06f26822932b38c488b653668e8c42d1
bagaszuhri <bagaszuhri@gmail.com>
2021-07-29 14:35:58 +0700

    + component todos dan empty

---
+++ counter-starter/src/components/Empty/index.js
@@ -0,0 +1,12 @@
+ import styles from './Empty.module.css'
+
+ const Empty = () => {
+     return(
+        <div className={styles.empty}>
+            Halo
+        </div>
+
+     )
+ }
+
+ export default Empty
